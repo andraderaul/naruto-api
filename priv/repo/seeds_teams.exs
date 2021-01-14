a8231873c5ada294f578c6c40506ebc8e1910f61
Raul Andrade <theandraderaul@gmail.com>
2021-01-13 23:48:19 -0300

    feat: add seeds teams

---
+++ priv/repo/seeds_teams.exs
@@ -0,0 +1,37 @@
+
+alias NarutoApi.Repo
+alias NarutoApi.Team
+
+Repo.insert!(%Team{
+  name: "Akagi Gang"
+})
+
+
+Repo.insert!(%Team{
+  name: "Akatsuki"
+})
+
+
+Repo.insert!(%Team{
+  name: "Allied Mothers Force"
+})
+
+
+Repo.insert!(%Team{
+  name: "Allied Shinobi Forces"
+})
+
+
+Repo.insert!(%Team{
+  name: "Ame_Orphans"
+})
+
+
+Repo.insert!(%Team{
+  name: "Analysis Team"
+})
+
+
+Repo.insert!(%Team{
+  name: "A–B Combo"
+})
