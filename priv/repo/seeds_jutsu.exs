b7bc1a84b8b3ef80962a15c989d0ebb317c7de41
Raul Andrade <theandraderaul@gmail.com>
2021-01-14 19:48:30 -0300

    feat: add seeds jutsu

---
+++ priv/repo/seeds_jutsu.exs
@@ -0,0 +1,306 @@
+alias NarutoApi.Repo
+alias NarutoApi.Jutsu
+
+Repo.insert!(%Jutsu{
+  name: "Zetsu: Gatsūga"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Abusōbu Sufia"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Kasoku Busō Kaiten"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Santōka"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Akurobatto"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Kongōriki: Fudō"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Kongō Rōheki"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Kongō Fūin: Enyō Fūsatsu"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Kongō Fūsa"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Kongōjutsu: Zugai Kudaki"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Zankō"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Zanzō Bunshin"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Monzetsutoge no Jutsu (Yōsuruni Kanchō)"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Kūraidan"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Kūraigeki"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Kūshūdan"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Kūsa Bōheki"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Akuta"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Yashabushi"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Shihōhappō Shuriken"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Issai Kaiku"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Tomogoroshi no Haikotsu"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Shinobi Rengōgun no Jutsu"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Amanohabaya"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Amaterasu"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Amaterasu: Nijū"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Amaterasu: Entenka"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Amaterasu: Zanka"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Ame no Ohabari"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Amenomihashira"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Amenominaka"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Amenosubaruboshinomikoto"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Amenosubaruboshinomikoto: Gyokkō"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Amenosubaruboshinomikoto: Ryūgū"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Amenotejikara"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Amenotejikara: Chidori"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Zōfuku Kuchiyose no Jutsu"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Setsudan no Kei"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Sora to Sakura to Daichi no Ikari"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Chikushōdō"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Ankorodō Hiden Ninjutsu: Mochiton: Mugen Shiratama"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Mekkyaku"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Kagerō Ninpō: Utakata"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Muga"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Anshōsui"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Rakanken"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Shukaku no Ude"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Suna no Yoroi"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Nenkin no Yoroi"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Yoroigui"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Geijutsu"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Ansatsu Jutsu"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Shinra Banshō Dōka no Jutsu"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Dōka: Iwafubuki"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Dōka: Ganseki Sensha"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Dōka: Sabaku Kyū"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Dōka: Sabaku Sōsō"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Shura no Kō"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Shuradō"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Sōma no Kō"
+})
+
+
+Repo.insert!(%Jutsu{
+  name: "Nadare Mai"
+})
