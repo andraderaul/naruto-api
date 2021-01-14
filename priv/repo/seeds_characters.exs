09a386b8c2e44dcf9527a71771d2d847dc0e6086
Raul Andrade <theandraderaul@gmail.com>
2021-01-13 22:45:37 -0300

    feat: add seeds characters

---
+++ priv/repo/seeds_characters.exs
@@ -0,0 +1,538 @@
+alias NarutoApi.Repo
+alias NarutoApi.Character
+
+Repo.insert!(%Character{
+  name: "Nadare Rōga",
+  kanji_name: "狼牙ナダレ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/1/19/Nadare_R%C5%8Dga.png/revision/latest?cb=20160116190200"
+})
+
+Repo.insert!(%Character{
+  name: "Nae",
+  kanji_name: "ナエ",
+  birthday: nil,
+  age: [19],
+  gender: "Female",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/e/e2/Nae.png/revision/latest?cb=20150725183041"
+})
+
+Repo.insert!(%Character{
+  name: "Nagare",
+  kanji_name: "ナガレ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/b/be/Nagare.png/revision/latest?cb=20150528132343"
+})
+
+Repo.insert!(%Character{
+  name: "Nagato",
+  kanji_name: "長門",
+  birthday: "September 19",
+  age: [35],
+  gender: "Male",
+  height: [175.5],
+  weight: [40.1],
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/4/46/Nagato.png/revision/latest?cb=20150821081644"
+})
+
+Repo.insert!(%Character{
+  name: "Naho",
+  kanji_name: "ナホ",
+  birthday: nil,
+  age: nil,
+  gender: "Female",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/4/48/Naho2.png/revision/latest?cb=20110918054525"
+})
+
+Repo.insert!(%Character{
+  name: "Naka Uchiha",
+  kanji_name: "うちはナカ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: [178.0],
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/a/ae/Naka_Uchiha.png/revision/latest?cb=20131114131356"
+})
+
+Repo.insert!(%Character{
+  name: "Namida Suzumeno",
+  kanji_name: "雀乃なみだ",
+  birthday: nil,
+  age: nil,
+  gender: "Female",
+  height: [141.0],
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/1/1d/Namida.png/revision/latest?cb=20171220103121"
+})
+
+Repo.insert!(%Character{
+  name: "Nan",
+  kanji_name: "ナン",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/3/3f/Nan.png/revision/latest?cb=20150302185626"
+})
+
+Repo.insert!(%Character{
+  name: "Nanafushi",
+  kanji_name: "ナナフシ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/1/14/Nanafushi.png/revision/latest?cb=20150626140801"
+})
+
+Repo.insert!(%Character{
+  name: "Nanara",
+  kanji_name: "ナナラ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture: nil
+})
+
+Repo.insert!(%Character{
+  name: "Nangō",
+  kanji_name: "ナンゴウ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/c/cf/Nango.PNG/revision/latest?cb=20170129112535"
+})
+
+Repo.insert!(%Character{
+  name: "Naori Uchiha",
+  kanji_name: "うちはナオリ",
+  birthday: nil,
+  age: nil,
+  gender: "Female",
+  height: [166.0],
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/a/a1/Naori_Uchiha_preview.png/revision/latest?cb=20200605182422"
+})
+
+Repo.insert!(%Character{
+  name: "Naraka Path (character)",
+  kanji_name: "地獄道",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: [185.0],
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/8/81/Naraka_Path.png/revision/latest?cb=20160116083050"
+})
+
+Repo.insert!(%Character{
+  name: "Naruto Musasabi",
+  kanji_name: nil,
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture: nil
+})
+
+Repo.insert!(%Character{
+  name: "Naruto Uzumaki",
+  kanji_name: "うずまきナルト",
+  birthday: "October 10",
+  age: [12, 13, 15, 17],
+  gender: "Male",
+  height: [145.3, 147.5, 166.0, 180.0],
+  weight: [40.1, 40.6, 50.9],
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/0/09/Naruto_newshot.png/revision/latest?cb=20170621101134"
+})
+
+Repo.insert!(%Character{
+  name: "Natori",
+  kanji_name: "ナトリ",
+  birthday: nil,
+  age: [24],
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/c/c6/Natori.png/revision/latest?cb=20160113170823"
+})
+
+Repo.insert!(%Character{
+  name: "Natsu",
+  kanji_name: "ナツ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/c/c7/Natsu_-_330.png/revision/latest?cb=20130914190022"
+})
+
+Repo.insert!(%Character{
+  name: "Natsu Hyūga",
+  kanji_name: "日向ナツ",
+  birthday: nil,
+  age: nil,
+  gender: "Female",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/2/24/Natsu_Hyuga.png/revision/latest?cb=20141204202830"
+})
+
+Repo.insert!(%Character{
+  name: "Natsuhi",
+  kanji_name: "ナツヒ",
+  birthday: nil,
+  age: nil,
+  gender: "Female",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/7/77/Natsuhi.png/revision/latest?cb=20150529111431"
+})
+
+Repo.insert!(%Character{
+  name: "Nattō Itohiki",
+  kanji_name: "糸引ナットウ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/4/4c/Natt%C5%8D_Itohiki.png/revision/latest?cb=20201103191411"
+})
+
+Repo.insert!(%Character{
+  name: "Nauma",
+  kanji_name: "ナウマ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/1/17/Nauma.png/revision/latest?cb=20150723204711"
+})
+
+Repo.insert!(%Character{
+  name: "Nawaki",
+  kanji_name: "縄樹",
+  birthday: "August 9",
+  age: [12],
+  gender: "Male",
+  height: [140.3],
+  weight: [37.2],
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/a/a5/Nawaki_Infobox.png/revision/latest?cb=20160630135236"
+})
+
+Repo.insert!(%Character{
+  name: "Nazuna",
+  kanji_name: "ナズナ",
+  birthday: nil,
+  age: nil,
+  gender: "Female",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/2/2b/Nazuna.png/revision/latest?cb=20161116232518"
+})
+
+Repo.insert!(%Character{
+  name: "Neji Hyūga",
+  kanji_name: "日向ネジ",
+  birthday: "July 3",
+  age: [13, 14, 17, 18],
+  gender: "Male",
+  height: [159.2, 160.1, 172.1],
+  weight: [45.8, 46.8, 54.2],
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/8/8e/Neji_Part_I_Screenshot.png/revision/latest?cb=20160118081119"
+})
+
+Repo.insert!(%Character{
+  name: "Nejiri",
+  kanji_name: "ネジリ",
+  birthday: nil,
+  age: [16],
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/4/4d/Nejiri.JPG/revision/latest?cb=20150807090437"
+})
+
+Repo.insert!(%Character{
+  name: "Nekobaa",
+  kanji_name: "猫バア",
+  birthday: nil,
+  age: nil,
+  gender: "Female",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/e/ee/Nekobaa.png/revision/latest?cb=20170204180154"
+})
+
+Repo.insert!(%Character{
+  name: "Nekomata",
+  kanji_name: "猫又",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/7/77/Nekomata.png/revision/latest?cb=20160217064819"
+})
+
+Repo.insert!(%Character{
+  name: "Nemui",
+  kanji_name: "ネムイ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture: nil
+})
+
+Repo.insert!(%Character{
+  name: "Nerugui",
+  kanji_name: nil,
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/9/9c/Nerugui.png/revision/latest?cb=20150212131159"
+})
+
+Repo.insert!(%Character{
+  name: "Nezumi Kaitō",
+  kanji_name: "怪盗ネズミ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/f/fa/Nezumi_Kait%C5%8D.png/revision/latest?cb=20190505232558"
+})
+
+Repo.insert!(%Character{
+  name: "Ni",
+  kanji_name: "弐",
+  birthday: nil,
+  age: nil,
+  gender: "Female",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/d/da/Ni2.png/revision/latest?cb=20160219231433"
+})
+
+Repo.insert!(%Character{
+  name: "Ni's Dogs",
+  kanji_name: nil,
+  birthday: nil,
+  age: nil,
+  gender: nil,
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/2/20/Ni%27s_Dogs.png/revision/latest?cb=20140803202906"
+})
+
+Repo.insert!(%Character{
+  name: "Nigai",
+  kanji_name: "ニガイ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/4/4e/Nigai2.png/revision/latest?cb=20160226233421"
+})
+
+Repo.insert!(%Character{
+  name: "Nine-Tailed Naruto Clone",
+  kanji_name: "九尾化したナルトのクローン",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/5/51/Kyuubi_Naruto_Clone.png/revision/latest?cb=20150722111913"
+})
+
+Repo.insert!(%Character{
+  name: "Ningame",
+  kanji_name: "忍亀",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: [90.0],
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/b/b9/Nin-game.png/revision/latest?cb=20160425233043"
+})
+
+Repo.insert!(%Character{
+  name: "Nishi",
+  kanji_name: "西",
+  birthday: "August 11",
+  age: [28],
+  gender: "Male",
+  height: [180.0],
+  weight: [65.0],
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/f/fc/Nishi.JPG/revision/latest?cb=20150807085116"
+})
+
+Repo.insert!(%Character{
+  name: "Nitora Take",
+  kanji_name: "武にとら",
+  birthday: nil,
+  age: nil,
+  gender: "Female",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/6/60/Nitora_Take.png/revision/latest?cb=20171209020321"
+})
+
+Repo.insert!(%Character{
+  name: "Nobori",
+  kanji_name: "ノボリ",
+  birthday: nil,
+  age: [10],
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/4/47/Nobori.png/revision/latest?cb=20150129192310"
+})
+
+Repo.insert!(%Character{
+  name: "Noji",
+  kanji_name: nil,
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture: nil
+})
+
+Repo.insert!(%Character{
+  name: "Nonota",
+  kanji_name: "ノノタ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/5/53/Nonota_HDShot.png/revision/latest?cb=20130117152213"
+})
+
+Repo.insert!(%Character{
+  name: "Nonō Yakushi",
+  kanji_name: "薬師ノノウ",
+  birthday: "March 5",
+  age: [31],
+  gender: "Female",
+  height: [155.0],
+  weight: [42.1],
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/4/4a/Nono_HD_shot.png/revision/latest?cb=20131024132535"
+})
+
+Repo.insert!(%Character{
+  name: "Nowaki",
+  kanji_name: "ノワキ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: [184.6],
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/a/a5/F%C5%ABshin.png/revision/latest?cb=20170105140650"
+})
+
+Repo.insert!(%Character{
+  name: "Nue",
+  kanji_name: "鵺",
+  birthday: nil,
+  age: nil,
+  gender: nil,
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/1/1c/Nue.png/revision/latest?cb=20170705112151"
+})
+
+Repo.insert!(%Character{
+  name: "Nurari",
+  kanji_name: "濡羅吏",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/c/c5/Nurari.PNG/revision/latest?cb=20170816204616"
+})
+
+Repo.insert!(%Character{
+  name: "Nurui",
+  kanji_name: "ヌルイ",
+  birthday: nil,
+  age: nil,
+  gender: "Male",
+  height: nil,
+  weight: nil,
+  picture:
+    "https://static.wikia.nocookie.net/naruto/images/c/c4/Nurui.png/revision/latest?cb=20160214202654"
+})
